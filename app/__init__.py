a8eed34af692cac2c7c3893530a2a7b316ae947e
GbAlves-sys <grc.alves2@gmail.com>
2025-02-19 18:37:26 +0000

    modified:   __pycache__/config.cpython-312.pyc 	new file:   __pycache__/manage.cpython-312.pyc 	modified:   app/__init__.py 	modified:   app/__pycache__/__init__.cpython-312.pyc 	new file:   app/__pycache__/auth.cpython-312.pyc 	new file:   app/__pycache__/models.cpython-312.pyc 	new file:   app/__pycache__/routes.cpython-312.pyc 	modified:   app/auth.py 	modified:   app/routes.py 	new file:   app/templates/home.html 	new file:   instance/app.db 	new file:   manage.py 	new file:   migrations/README 	new file:   migrations/__pycache__/env.cpython-312.pyc 	new file:   migrations/alembic.ini 	new file:   migrations/env.py 	new file:   migrations/script.py.mako 	new file:   migrations/versions/24480e57e5a5_initial_migration.py 	new file:   migrations/versions/__pycache__/24480e57e5a5_initial_migration.cpython-312.pyc 	modified:   requirements.txt 	modified:   run.py 	modified:   venv/bin/activate 	modified:   venv/pyvenv.cfg

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager  # Adicione essa importação
 from config import Config
 
 db = SQLAlchemy()
+login_manager = LoginManager()  # Inicializa o LoginManager
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    login_manager.init_app(app)  # Inicializa o LoginManager no app
 
+    # Define a rota de login padrão para redirecionamento
+    login_manager.login_view = "auth.login"
+
+    # Importa e registra os blueprints
     from app.auth import auth_bp
+    from app.routes import main_bp
     app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
 
     return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models import User  # Importação dentro da função para evitar import circular
+    return User.query.get(int(user_id))  # Retorna o usuário pelo ID
